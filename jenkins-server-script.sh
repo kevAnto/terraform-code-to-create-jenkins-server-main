50987c71b6a696859b95ea50c82bc52e7eebc0cb
Antoine CHOULA <76167680+kevAnto@users.noreply.github.com>
2023-08-16 08:33:11 +0100

    Update jenkins-server-script.sh

--- jenkins-server-script.sh
+++ jenkins-server-script.sh
@@ -2,12 +2,13 @@
 
 # install jenkins
 
-sudo yum update
+sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
 sudo amazon-linux-extras install java-openjdk11 -y
+sudo dnf install java-11-amazon-corretto -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
